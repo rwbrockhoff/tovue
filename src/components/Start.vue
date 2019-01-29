7d82a04d44ecaa9b63925305151eafe8494feb32
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-01-29 11:45:59 -0700

    fix: uri path corrected.

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,40 +1,35 @@
 <template>
-    <div class='start'>
-        <h1> akt </h1>
-        <button @click='login' type="button" class="btn btn-light">Login/Sign Up</button>
-    </div>
+  <div class="start">
+    <h1>akt</h1>
+    <button @click="login" type="button" class="btn btn-light">Login/Sign Up</button>
+  </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import { mapState } from "vuex";
 export default {
-    computed: mapState([
-        'bike'
-    ]),
-    methods: {
-        login(){
-            let {VUE_APP_AUTH0_DOMAIN,
-                VUE_APP_AUTH0_CLIENT_ID
-            } = process.env;
-            
-  let url = `${encodeURIComponent(window.location.origin)}/auth/callback`
+  computed: mapState(["bike"]),
+  methods: {
+    login() {
+      let { VUE_APP_AUTH0_DOMAIN, VUE_APP_AUTH0_CLIENT_ID } = process.env;
 
-  window.location = `https://${VUE_APP_AUTH0_DOMAIN}/authorize?client_id=${VUE_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_url=${url}&response_type=code`
-        }
-    },
-   
-}
+      let url = `${encodeURIComponent(window.location.origin)}/auth/callback`;
+
+      window.location = `https://${VUE_APP_AUTH0_DOMAIN}/authorize?client_id=${VUE_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`;
+    }
+  }
+};
 </script>
 
 <style scoped>
 div.start {
-    height: 80vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 h1 {
-    color: white;
+  color: white;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -12,20 +12,11 @@ export default new Router({
       path: "/",
       name: "start",
       component: Start
-  },
+    },
     {
       path: "/akt",
       name: "akt",
       component: Akt
     }
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
-    // }
   ]
 });
